dbce76731c437114d53c862bdcbfbb970ee45712
iradbouzidi <irad.bouzidi@esprit.tn>
2021-03-15 16:13:32 +0000

    done

--- facial_with_email.py
+++ facial_with_email.py
@@ -1,74 +1,86 @@
-#! /usr/bin/python
-
 # import the necessary packages
-from imutils.video import VideoStream
-from imutils.video import FPS
 import face_recognition
-import imutils
 import pickle
 import time
 import cv2
-import email, smtplib, ssl
+import os
+
+import imutils
+from imutils.video import VideoStream
+from imutils.video import FPS
+
+import smtplib
+import ssl
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-#Initialize 'currentname' to trigger only when a new person is identified.
+import boto3
+import datetime
+from datetime import datetime
+
+# Initialize 'currentname' to trigger only when a new person is identified.
 currentname = "unknown"
-#Determine faces from encodings.pickle file model created from train_model.py
+# Determine faces from encodings.pickle file model created from train_model.py
 encodingsP = "encodings.pickle"
-#use this xml file
+# use this xml file
 cascade = "haarcascade_frontalface_default.xml"
 
-#function for setting up emails
-def send_mail(name,img_name):
-	subject = "Raspberry Pi Cam"
-	body = f"""
+date_time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+if not os.path.exists('captured'):
+    os.makedirs('captured')
+
+# function for setting up emails
+
+
+def send_mail(name, img_name):
+    subject = "Raspberry Pi Cam"
+    body = f"""
 		{name} is at the door!
 	"""
-	sender_email = "irad.bouzidi@esprit.tn"
-	receiver_email = "b.irad1994@gmail.com"
-	password = "ab.s.h@94"
+    sender_email = "irad.bouzidi@esprit.tn"
+    receiver_email = "b.irad1994@gmail.com"
+    password = "ab.s.h@94"
 
-	# Create a multipart message and set headers
-	message = MIMEMultipart()
-	message["From"] = sender_email
-	message["To"] = receiver_email
-	message["Subject"] = subject
-	message["Bcc"] = receiver_email  # Recommended for mass emails
+    # Create a multipart message and set headers
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject
+    message["Bcc"] = receiver_email  # Recommended for mass emails
 
-	# Add body to email
-	message.attach(MIMEText(body, "plain"))
+    # Add body to email
+    message.attach(MIMEText(body, "plain"))
 
-	filename = img_name  # In same directory as script
+    filename = img_name  # In same directory as script
 
-	# Open PDF file in binary mode
-	with open(filename, "rb") as attachment:
-    	# Add file as application/octet-stream
-    	# Email client can usually download this automatically as attachment
-		part = MIMEBase("application", "octet-stream")
-		part.set_payload(attachment.read())
+    # Open PDF file in binary mode
+    with open("captured/"+filename, "rb") as attachment:
+        # Add file as application/octet-stream
+        # Email client can usually download this automatically as attachment
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
 
-	# Encode file in ASCII characters to send by email    
-	encoders.encode_base64(part)
+    # Encode file in ASCII characters to send by email
+    encoders.encode_base64(part)
 
-	# Add header as key/value pair to attachment part
-	part.add_header(
-    	"Content-Disposition",
-    	f"attachment; filename= {filename}",
-	)
+    # Add header as key/value pair to attachment part
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {img_name}",
+    )
 
-	# Add attachment to message and convert message to string
-	message.attach(part)
-	text = message.as_string()
+    # Add attachment to message and convert message to string
+    message.attach(part)
+    text = message.as_string()
 
-	# Log in to server using secure context and send email
-	context = ssl.create_default_context()
-	with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-		server.login(sender_email, password)
-		server.sendmail(sender_email, receiver_email, text)
-    
+    # Log in to server using secure context and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, text)
+
 
 # load the known faces and embeddings along with OpenCV's Haar
 # cascade for face detection
@@ -87,98 +99,114 @@ fps = FPS().start()
 
 # loop over frames from the video file stream
 while True:
-	# grab the frame from the threaded video stream and resize it
-	# to 500px (to speedup processing)
-	frame = vs.read()
-	frame = imutils.resize(frame, width=500)
-	
-	# convert the input frame from (1) BGR to grayscale (for face
-	# detection) and (2) from BGR to RGB (for face recognition)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # grab the frame from the threaded video stream and resize it
+    # to 500px (to speedup processing)
+    frame = vs.read()
+    frame = imutils.resize(frame, width=500)
 
-	# detect faces in the grayscale frame
-	rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
-		minNeighbors=5, minSize=(30, 30),
-		flags=cv2.CASCADE_SCALE_IMAGE)
+    # convert the input frame from (1) BGR to grayscale (for face
+    # detection) and (2) from BGR to RGB (for face recognition)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-	# OpenCV returns bounding box coordinates in (x, y, w, h) order
-	# but we need them in (top, right, bottom, left) order, so we
-	# need to do a bit of reordering
-	boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
+    # detect faces in the grayscale frame
+    rects = detector.detectMultiScale(gray, scaleFactor=1.1,
+                                      minNeighbors=5, minSize=(30, 30),
+                                      flags=cv2.CASCADE_SCALE_IMAGE)
 
-	# compute the facial embeddings for each face bounding box
-	encodings = face_recognition.face_encodings(rgb, boxes)
-	names = []
+    # OpenCV returns bounding box coordinates in (x, y, w, h) order
+    # but we need them in (top, right, bottom, left) order, so we
+    # need to do a bit of reordering
+    boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
 
-	# loop over the facial embeddings
-	for encoding in encodings:
-		# attempt to match each face in the input image to our known
-		# encodings
-		matches = face_recognition.compare_faces(data["encodings"],
-			encoding)
-		name = "Unknown"
+    # compute the facial embeddings for each face bounding box
+    encodings = face_recognition.face_encodings(rgb, boxes)
+    names = []
 
-		# check to see if we have found a match
-		if True in matches:
-			# find the indexes of all matched faces then initialize a
-			# dictionary to count the total number of times each face
-			# was matched
-			matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-			counts = {}
+    # loop over the facial embeddings
+    for encoding in encodings:
+        # attempt to match each face in the input image to our known
+        # encodings
+        matches = face_recognition.compare_faces(data["encodings"],
+                                                 encoding)
+        name = "Unknown"
 
-			# loop over the matched indexes and maintain a count for
-			# each recognized face face
-			for i in matchedIdxs:
-				name = data["names"][i]
-				counts[name] = counts.get(name, 0) + 1
+        # check to see if we have found a match
+        if True in matches:
+            # find the indexes of all matched faces then initialize a
+            # dictionary to count the total number of times each face
+            # was matched
+            matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+            counts = {}
 
-			# determine the recognized face with the largest number
-			# of votes (note: in the event of an unlikely tie Python
-			# will select first entry in the dictionary)
-			name = max(counts, key=counts.get)
-			
-			#If someone in your dataset is identified, print their name on the screen
-			if currentname != name:
-				currentname = name
-				print(currentname)
-				#Take a picture to send in the email
-				img_name = "image.jpg"
-				time.sleep(2)
-				print('Taking a picture.')
-				cv2.imwrite(img_name, frame)
-				
-				
-				#Now send me an email to let me know who is at the door
-				send_mail(name,img_name)
-		
-		# update the list of names
-		names.append(name)
+            # loop over the matched indexes and maintain a count for
+            # each recognized face face
+            for i in matchedIdxs:
+                name = data["names"][i]
+                counts[name] = counts.get(name, 0) + 1
 
-	# loop over the recognized faces
-	for ((top, right, bottom, left), name) in zip(boxes, names):
-		# draw the predicted face name on the image - color is in BGR
-		cv2.rectangle(frame, (left, top), (right, bottom),
-			(0, 255, 225), 2)
-		y = top - 15 if top - 15 > 15 else top + 15
-		cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-			.8, (0, 255, 255), 2)
+            # determine the recognized face with the largest number
+            # of votes (note: in the event of an unlikely tie Python
+            # will select first entry in the dictionary)
+            name = max(counts, key=counts.get)
 
-	# display the image to our screen
-	cv2.imshow("Facial Recognition is Running", frame)
-	key = cv2.waitKey(1) & 0xFF
+            # If someone in your dataset is identified, print their name on the screen
+            if currentname != name:
+                currentname = name
+                print(currentname)
+                person = currentname
+                # Take a picture to send in the email
+                time_now = datetime.now().strftime('%b %d %Y %H:%M:%S')
 
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+                img_name = person + " - " + time_now + ".jpg"
 
-	# update the FPS counter
-	fps.update()
+                time.sleep(2)
+                print('Taking a picture.')
+                cv2.imwrite("captured/" + img_name, frame)
+
+                # Now send me an email to let me know who is at the door
+                send_mail(name, img_name)
+
+        # update the list of names
+
+        names.append(name)
+
+    # loop over the recognized faces
+    for ((top, right, bottom, left), name) in zip(boxes, names):
+        # draw the predicted face name on the image - color is in BGR
+        cv2.rectangle(frame, (left, top), (right, bottom),
+                      (0, 255, 225), 2)
+        y = top - 15 if top - 15 > 15 else top + 15
+        cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+                    .8, (0, 255, 255), 2)
+
+    # display the image to our screen
+    cv2.imshow("Facial Recognition is Running", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
+
+    # update the FPS counter
+    fps.update()
 
 # stop the timer and display FPS information
 fps.stop()
 print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
 print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+now = datetime.now()
+
+date_time = now.strftime("%b %d %Y %H:%M:%S")
+Id = int(now.strftime("%Y%d%m%H%M%S"))
+dynamodb.Table(currentname.lower()).put_item(
+    Item={
+        'Id': Id,
+        'Name': f'{currentname.lower()}',
+        "Time": f'{date_time}',
+    }
+)
 
 # do a bit of cleanup
 cv2.destroyAllWindows()
--- headshots_picam.py
+++ headshots_picam.py
@@ -3,6 +3,7 @@ import os
 from os import path
 from picamera import PiCamera
 from picamera.array import PiRGBArray
+import boto3
 
 cam = PiCamera()
 cam.resolution = (512, 304)
@@ -10,31 +11,59 @@ cam.framerate = 10
 rawCapture = PiRGBArray(cam, size=(512, 304))
 
 name = input("Give person name: ")
-if not path.exists("dataset/"+name):
+if not path.exists("dataset/" + name):
     os.mkdir("dataset/" + name)
 img_counter = len(os.listdir("dataset/" + name))
+
+
+def create_table():
+    try:
+        dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+
+        dynamodb.create_table(
+            TableName=f'{name}',
+            KeySchema=[
+                {
+                    'KeyType': 'HASH',
+                    'AttributeName': 'Id'
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'Id',
+                    'AttributeType': 'N'
+                },
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 10,
+                'WriteCapacityUnits': 10
+            }
+        )
+    except Exception:
+        pass
+
 
 while True:
     for frame in cam.capture_continuous(rawCapture, format="bgr", use_video_port=True):
         image = frame.array
         cv2.imshow("Press Space to take a photo", image)
         rawCapture.truncate(0)
-    
+
         k = cv2.waitKey(1)
         rawCapture.truncate(0)
-        
-        if k%256 == 27: # ESC pressed
+
+        if k % 256 == 27:  # ESC pressed
             break
-        elif k%256 == 32:
+        elif k % 256 == 32:
             # SPACE pressed
-            img_name = "dataset/"+ name +"/image_{}.jpg".format(img_counter)
+            img_name = "dataset/" + name + "/image_{}.jpg".format(img_counter)
             img_counter += 1
             cv2.imwrite(img_name, image)
             print(f"{img_name} written!")
-            
-            
-    if k%256 == 27:
+
+    if k % 256 == 27:
         print("Escape hit, closing...")
+        create_table()
         break
 
 cv2.destroyAllWindows()
